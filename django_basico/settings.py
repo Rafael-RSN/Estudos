0b7cd5c10a9153ba87440a903746baf5a6c5607d
Rafael-RSN <rsn.rafael90@gmail.com>
2025-11-04 02:11:58 -0300

    virgula

--- django_basico/settings.py
+++ django_basico/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'produtos',
+    'produtos'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'America/Brasilia'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
